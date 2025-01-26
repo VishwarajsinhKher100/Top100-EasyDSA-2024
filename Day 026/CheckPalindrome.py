e899c98992d7c8f7735fde3b06554ec240cae7b0
Vishwarajsinh Kher <vishwarajsinh.kher100@gmail.com>
2025-01-26 19:49:10 +0530

    Add files via upload
    day 26 python file

---
+++ Day 026/CheckPalindrome.py
@@ -0,0 +1,14 @@
+def is_palindrome_recursive(s):
+    if len(s) <= 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return is_palindrome_recursive(s[1:-1])
+
+# Example Usage
+string = input("Ente the string: ")
+if is_palindrome_recursive(string):
+    print(f'"{string}" is a palindrome.')
+else:
+    print(f'"{string}" is not a palindrome.')
+
